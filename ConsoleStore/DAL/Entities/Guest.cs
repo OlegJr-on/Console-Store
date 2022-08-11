3774c75ae27c74c12aa5f651f8dca753657593b6
Oleh Mandra <mandra9012003@gmail.com>
2022-08-11 16:53:26 +0300

    (Entities) Created Guest entity

---
+++ ConsoleStore/DAL/Entities/Guest.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Data;
+
+namespace DAL.Entities
+{
+    public class Guest : UserEntity
+    {
+        public Guest(int id) : base(id, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty)
+        { }
+
+        public override IAccessRole GetAccess()
+            => new GuestAccess();
+    }
+}
