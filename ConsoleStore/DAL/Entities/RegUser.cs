f713015631a1d56c6a24a993d6493f66d147c61b
Oleh Mandra <mandra9012003@gmail.com>
2022-08-11 16:52:40 +0300

    (Entities) Created RegUser entity

---
+++ ConsoleStore/DAL/Entities/RegUser.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Data;
+using System.Collections.Generic;
+
+namespace DAL.Entities
+{
+    public class RegUser : UserEntity
+    {
+        public RegUser(int id, string name, string surname, string location, string email, string password, string phone)
+            : base(id, name, surname, location, email, password, phone)
+        {
+            base.OrderList = new List<Order>();
+        }
+
+        public override IAccessRole GetAccess()
+             => new RegisteredUsersAccess();
+    }
+}
