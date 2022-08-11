a723b12fedaa06f981dff0a10a2f0f7036109cb3
Oleh Mandra <mandra9012003@gmail.com>
2022-08-11 18:56:51 +0300

    (Services) Added Registered user  service

---
+++ ConsoleStore/BLL/Services/RegUserService.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Entities;
+using DAL.Data;
+
+namespace BLL.Services
+{
+    public class RegUserService : BaseService
+    {
+
+        public RegUserService(RegUser user) : base(user)
+        {
+            base.LoggedUsers = new RegUserRepository();
+        }
+    }
+}
