34fe16cc9c4c8ed81acf4b6c7109779e57ea71fe
Oleh Mandra <mandra9012003@gmail.com>
2022-08-11 17:07:29 +0300

    (Data) Added interface IRepository which realize pattern repository

---
+++ ConsoleStore/DAL/Data/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.Data
+{
+
+    interface IRepository<T>
+    {
+        void Add(T entity);
+        string Update(T entity, string change, string NewParametr);
+        T GetById(int id);
+        void DeleteById(int id);
+    }
+
+}
