9ca45bd2345e79cdbbba32d6a93508777ca10beb
Oleh Mandra <mandra9012003@gmail.com>
2022-08-11 19:15:34 +0300

    (Services) Added admin  service

---
+++ ConsoleStore/BLL/Services/AdminService.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Entities;
+using DAL.Data;
+
+namespace BLL.Services
+{
+    public class AdminService : BaseService
+    {
+
+        public AdminService(Admin user) : base(user)
+        {
+            base.LoggedUsers = new RegUserRepository();
+        }
+    }
+}
