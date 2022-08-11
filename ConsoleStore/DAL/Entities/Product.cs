7134169b8c40a3555784ee387e34fca96e3b32e7
Oleh Mandra <mandra9012003@gmail.com>
2022-08-11 16:54:06 +0300

    (Entities) Created Product entity

---
+++ ConsoleStore/DAL/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿
+namespace DAL.Entities
+{
+    public class Product : BaseEntities
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Manufacturer { get; set; }
+        public double Price { get; set; }
+        public string Comment { get; set; }
+
+        public Product(int id, string title, string category, string manufacture, double price, string comment)
+        => (base.Id, Title, Category, Manufacturer, Price, Comment) = (id, title, category, manufacture, price, comment);
+
+        public override string ToString()
+                => $"\n\t\t*{Title}*\n\t+ Category product: {Category}\n\t" +
+                        $"+ Production: {Manufacturer}\n\t+ Description: {Comment}" +
+                        $"\n\t+ Price: {Price} UAH";
+    }
+}
