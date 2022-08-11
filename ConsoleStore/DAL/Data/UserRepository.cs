b0339e6c7df2472709c9761a14719caf6f657a14
Oleh Mandra <mandra9012003@gmail.com>
2022-08-11 17:10:31 +0300

    (Data) Created abstact class "UserRepository"

---
+++ ConsoleStore/DAL/Data/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Data
+{
+    public abstract class UserRepository<T> : IUserIsExist
+    {
+        public List<T> _UserList { get; set; }
+
+        public abstract T SearchUser(string email, string password);
+
+        public abstract bool UserIsExist(string email, string password);
+    }
+}
