dae2820535bdfb0d942f2094565299966ae7d9b6
Oleh Mandra <mandra9012003@gmail.com>
2022-08-11 18:56:03 +0300

    (Services) Added Guest service

---
+++ ConsoleStore/BLL/Services/GuestService.cs
@@ -0,0 +1,12 @@
+﻿using DAL.Entities;
+using DAL.Data;
+
+namespace BLL.Services
+{
+    public class GuestService : BaseService
+    {
+        public GuestService(Guest user) : base(user)
+        { }
+
+    }
+}
