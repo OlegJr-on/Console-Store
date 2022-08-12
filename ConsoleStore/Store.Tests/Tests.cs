5b094267b6f28ab445c4ed4bbd66b48a44dc7d6a
Oleh Mandra <mandra9012003@gmail.com>
2022-08-12 13:42:29 +0300

    (Tests) Added testing for authorization act

--- ConsoleStore/Store.Tests/Tests.cs
+++ ConsoleStore/Store.Tests/Tests.cs
@@ -31,4 +31,75 @@ namespace Store.Tests
         }
     }
 
+
+    [TestFixture]
+    public class Class_Testing_Authorization
+    {
+        [TestCase("kpxo", "lxpkco", false)]
+        [TestCase("fegor77@gmail.com", "Inga$55", true)]
+        [TestCase("petka@gmail.com", "Shot69+#", true)]
+        [TestCase(null, "Pswrd077--*", false)]
+        public void CheckCtorAuthorization_EmailPassword_ShouldReturnTrueOrFalse(string email, string password, bool expected)
+        {
+            //arrange
+            var authorization = new Authorization(email, password);
+            bool actual = authorization.IsEntranceCorrect;
+
+            //assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase("Frodo", "Begenns", "Shire,Middle-earth", "frod99@gmail.com", "PasS099&*", "047-633-2222", true)]
+        [TestCase(null, "null", "_", "gmail.com", "nghjkl*", "6669933-2222", false)]
+        [TestCase("Bill", "Marlin", "London,UK", "bil@gmail.com", "N66ghjkl*", "6549865", false)]
+        public void CheckCtorAuthorization_Registration_ShouldReturnTrueOrFalse(string name, string surname, string location, string email,
+                                                                            string password, string phone, bool expected)
+        {
+            //arrange
+            var authorization = new Authorization(new Registration(name, surname, location, email, password, phone));
+            var actual = authorization.IsEntranceCorrect;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void CheckEmptyCtorAuthorization_ShouldReturnGuestMode()
+        {
+            //arrange
+            var authorization = new Authorization();
+            bool result = false;
+
+            //act
+            if (authorization.AuthorizedUser is GuestService)
+            {
+                result = true;
+            }
+
+            //assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void CheckSignOutAccount_ShouldReturnNewAuthorizationMode()
+        {
+            //arrange
+            var authorization = new Authorization("fegor77@gmail.com", "Inga$55");
+            bool result = false;
+
+            //act
+            if (authorization.IsEntranceCorrect && authorization.AuthorizedUser is RegUserService)
+                authorization = authorization.SignOutAccount();
+
+
+            if (authorization.AuthorizedUser is GuestService)
+                result = true;
+
+            //assert
+            Assert.IsTrue(result);
+        }
+    }
+
+
+
 }
