4e156a935539139cb4c74f7a8c23e7c32ba77241
Oleh Mandra <mandra9012003@gmail.com>
2022-08-12 13:41:11 +0300

    (Tests) Added testing for registration act

---
+++ ConsoleStore/Store.Tests/Tests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using DAL.Entities;
+using DAL.Data;
+using BLL.Services;
+using BLL;
+using System.Text;
+
+namespace Store.Tests
+{
+    [TestFixture]
+    public class Class_Testing_Registration
+    {
+        [TestCase("Frodo", "Begenns", "Shire,Middle-earth", "frod99@gmail.com", "PasS099&*", "047-633-2222")]
+        [TestCase(null, "06", "Kyiv,Ukraine", "lolo9@gmail.com", "nghjkl*", "666-633-2222")]
+        [TestCase("Nill", "Johnson", "London,UK", "nil@gmail.com", "N66ghjkl*", "6549865")]
+        public void CheckUserRegistration_NewUserData_ShouldReturnTrueOrFalse(string name, string surname, string location, string email,
+                                                                            string password, string phone)
+        {
+            //arrange
+            var actual = new Registration(name, surname, location, email, password, phone);
+            bool result = false;
+
+            //act
+            if (actual.users.GetUserList().Contains(actual.GetUser()))
+                result = true;
+
+            //assert
+            Assert.That(result, Is.True, message: "Registration was not successful");
+        }
+    }
+
+}
